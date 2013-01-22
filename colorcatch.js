881fac12dfce0c07a8475ea9575332751f6fecaa
Tony Zhang <tzhang@cmu-831539.wv.cc.cmu.edu>
2013-01-22 14:49:12 -0500

    separated code for bucket into separate bucket.js

--- colorcatch.js
+++ colorcatch.js
@@ -1,14 +1,16 @@
-var colorCatch = function() {
-    /* Canvas Global Variables */
-    var canvas;
-    var ctx;
-    var intId;
+/* Canvas Global Variables */
+var canvas;
+var ctx;
+var intId;
 
-    /* Game Global Variables */
-    var g = {
-        bucketX: 400,
-        bucketY: 250
-    };
+/* Game Global Variables */
+var g = {
+    bucketX: 400,
+    bucketY: 250,
+    squares: []
+};
+
+var colorCatch = function() {
 
     /* Initialize the game */
     this.init = function() {
@@ -33,28 +35,9 @@ var colorCatch = function() {
         // new boxes will be run here
     }
 
-    function drawBucket(x, y) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(x, y, 50, 60);
-    }
-
     function drawBackground() {
         ctx.fillStyle = "black";
         ctx.fillRect(0,0, canvas.width, canvas.height);
-    }
-
-    function getMousePosition(evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    }
-
-    function updateBucketPosition(evt) {
-        var mousePosition = getMousePosition(evt);
-        g.bucketX = mousePosition.x - 25;
-        g.bucketY = mousePosition.y - 30;
     }
 
     /* Setup event listeners for the game */
